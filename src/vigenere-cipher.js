5375a2a8e25326ff2d6122a1e0016445cc332115
Alexey <64707536+KhazanAlexey@users.noreply.github.com>
2022-03-22 00:23:34 +0300

    add: vigenere-cipher.js what-season.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,91 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(direction = true) {
+        this.direction = direction;
+        this.alphabel = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    }
+
+    keyProccessing(message, key) {
+        let count = Math.round((message.length - key.length) / key.length) + 1;
+        let newKey = key.repeat(count);
+        newKey = newKey.split('');
+        message = message.toUpperCase();
+        for (let i = 0; i < message.length; i++) {
+            if (this.alphabel.indexOf(message[i]) == -1) {
+                newKey.splice(i, 0, message[i])
+            }
+        }
+        newKey = newKey.slice(0, message.length).join('').toUpperCase();
+        return {corMessage: message, newKey: newKey};
+
+    }
+
+    encrypt(message, key) {
+        if (!message || !key) throw new Error("Incorrect arguments!");
+        let correctData = this.keyProccessing(message, key),
+            corMessage = correctData.corMessage,
+            newKey = correctData.newKey,
+            arr = [];
+
+        for (let i = 0; i < corMessage.length; i++) {
+            if (this.alphabel.indexOf(corMessage[i]) != -1) {
+                let newLetterIndex = (this.alphabel.indexOf(corMessage[i]) + this.alphabel.indexOf(newKey[i])) % 26;
+                arr.push(this.alphabel[newLetterIndex]);
+            } else {
+                arr.push(corMessage[i]);
+            }
+        }
+        if (this.direction) {
+            return arr.join('');
+        } else {
+            return arr.reverse().join('');
+        }
+
+    }
+
+    decrypt(message, key) {
+        if (!message || !key) throw new Error("Incorrect arguments!");
+        let correctData = this.keyProccessing(message, key),
+            corMessage = correctData.corMessage,
+            newKey = correctData.newKey,
+            arr = [];
+
+        for (let i = 0; i < corMessage.length; i++) {
+            if (this.alphabel.indexOf(corMessage[i]) != -1) {
+                let newLetterIndex = (this.alphabel.indexOf(corMessage[i]) + 26 - this.alphabel.indexOf(newKey[i])) % 26;
+                arr.push(this.alphabel[newLetterIndex]);
+            } else {
+                arr.push(corMessage[i]);
+            }
+        }
+        if (this.direction) {
+            return arr.join('');
+        } else {
+            return arr.reverse().join('');
+        }
+    }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,66 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+    if (!date) return 'Unable to determine the time of year!'
+    if ((date instanceof Date === false || Object.getOwnPropertyNames(date).length > 0) && arguments.length > 0) {
+        throw new Error('Invalid date!');
+    }
+    if (date instanceof Date && Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date) && Object.keys(date).length === 0) {
+        let poraGoda
+
+        // executes, because `x` is technically a date object
+        const month = date.getMonth()
+        if(month==0||month==1||month==11){
+            poraGoda= 'winter'
+        }
+        if (2 <= month && month <= 4) {
+            poraGoda = 'spring';
+        }
+        if (5 <= month && month <= 7) {
+            poraGoda = 'summer';
+        }
+        if (8 <= month && month <= 10) {
+            poraGoda = 'fall';
+        }
+
+        return poraGoda
+    }
+    if (!(date instanceof Date)) {
+        throw new Error('Invalid date!')
+    }
+
+    function isValidDate(value) {
+        var dateWrapper = new Date(value);
+        return !isNaN(dateWrapper.getDate()) && Object.prototype.toString.call(dateWrapper) === '[object Date]';
+    }
+
+    if (!isValidDate(date)) {
+        throw new Error("Invalid date!")
+    }
+    if (date.constructor.name !== 'Date') {
+        throw new Error("Invalid date!")
+    }
+    if (Object.keys(date).length !== Object.keys(new Date())) {
+        throw new Error("Invalid date!")
+    }
+    if ((date).getMonth() === undefined) {
+        throw new Error("Invalid date!")
+    }
+    throw new Error('Invalid date!')
 }
 
 module.exports = {
-  getSeason
+    getSeason
 };
