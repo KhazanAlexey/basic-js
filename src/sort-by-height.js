4af7e60f626f62c2be1cb045cc87edfd8cf19023
Alexey <64707536+KhazanAlexey@users.noreply.github.com>
2022-03-30 23:10:56 +0300

    add: sort by height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -10,12 +10,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
- */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ //  */
+// 1. Создать отфильтрованный массив, который будет содержать { value: el, index: i }. Содержит нахождения -1 и его индекс
+// 2. Сортировать и применить фильтр к arr, чтобы не было -1
+// 3. Перебрать отфильтрованный массив, применить к arr метод splice, чтобы вставить -1 в то место где оно было изначально
+function sortByHeight(arr) {
+    const arrInadexValue = arr.map((el, i) => ({
+        value: el, index: i
+    }))
+    const arrNimOne=arrInadexValue.filter(el => el.value == -1)
+
+    const arrNoMinusOne = arrInadexValue.filter(el => el.value !== -1).sort((a, b) => a.value - b.value)
+
+
+    arrNimOne.forEach(function (el) {
+        arrNoMinusOne.splice(el.index,0,el)
+
+    })
+
+    return arrNoMinusOne.map(el=>el.value)
 }
 
 module.exports = {
-  sortByHeight
+    sortByHeight
 };
